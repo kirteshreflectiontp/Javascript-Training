fa4cb3b09c640b5f1829aef04bf95f7f738ae377
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-07-18 23:49:38 +0530

    calculator

---
+++ JS(18072022)/calculator.js
@@ -0,0 +1,30 @@
+function addition(){
+    var number1 = document.getElementById('number1').value;
+    var number2 = document.getElementById('number2').value;
+    sum = parseInt(number1) + parseInt(number2);
+    document.getElementById('result').innerHTML = sum;   
+}
+function substraction(){
+    var number1 = document.getElementById('number1').value;
+    var number2 = document.getElementById('number2').value;
+    sum = number1 - number2;
+    document.getElementById('result').innerHTML = sum;   
+}
+function multiplication(){
+    var number1 = document.getElementById('number1').value;
+    var number2 = document.getElementById('number2').value;
+    sum = number1 * number2;
+    document.getElementById('result').innerHTML = sum;   
+}
+function division(){
+    var number1 = document.getElementById('number1').value;
+    var number2 = document.getElementById('number2').value;
+    sum = number1 / number2;
+    document.getElementById('result').innerHTML = sum;   
+}
+function clearscreen(){
+    var number1 = document.getElementById('number1').value;
+    var number2 = document.getElementById('number2').value;
+    sum = '';
+    document.getElementById('result').innerHTML = sum
+}
