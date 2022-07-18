7aee69d86de3cbca752774b945f598c5814f1746
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-07-18 13:55:30 +0530

    intro.js

---
+++ JS(18072022)/intro.js
@@ -0,0 +1,15 @@
+var value1 = 200;
+var value2 = 300;
+var value3 = 20;
+var value4 = 10;
+var add = value1 + value2;
+var minus = value2 - value1;
+var multiply = value3 * value4;
+var divide = value1 / value3;
+var QE = (value1 + value3) * (value2 - value3)
+
+document.write('Addition:',add,'</br>');
+document.write('Substraction:',minus,'</br>');
+document.write('Multiplication:',multiply,'</br>');
+document.write('Division:',divide,'</br>');
+document.write('Quadratic Equation:',QE,'</br>');
