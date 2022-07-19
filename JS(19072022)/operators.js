68bcd19f030cf4138ea7d749f23fefbfacbb2bcd
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-07-20 02:14:52 +0530

    basic javascript assignment

--- JS(19072022)/operators.js
+++ JS(19072022)/operators.js
@@ -13,12 +13,7 @@ function lessthan(){
 function equalsto(){
     var number1 = parseInt(document.getElementById('number1').value);
     var number2 = parseInt(document.getElementById('number2').value);
-    var result = (number1 != number2) ? number1 + 'is  equals to' + number2 : number2 + 'is  equals to' + number1;
+    var result =  (number1 == number2) ? number1 + 'is equals to' + number2 : number1 + 'is not equals to' + number2;
     document.getElementById('result').innerHTML = result;
 }
-function notequalsto(){
-    var number1 = parseInt(document.getElementById('number1').value);
-    var number2 = parseInt(document.getElementById('number2').value);
-    var result = (number1 != number2) ? number1 + 'is not equals to' + number2 : number2 + 'is not equals to' + number1;
-    document.getElementById('result').innerHTML = result;
-}
+
