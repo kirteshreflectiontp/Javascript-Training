025d5760ae27f2bd4d43c18027f7bd09520a38ee
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-07-19 10:46:04 +0530

    calculator

--- JS(18072022)/calculator.js
+++ JS(18072022)/calculator.js
@@ -23,8 +23,8 @@ function division(){
     document.getElementById('result').innerHTML = sum;   
 }
 function clearscreen(){
-    var number1 = document.getElementById('number1').value;
-    var number2 = document.getElementById('number2').value;
+     document.getElementById('number1').value = '';
+     document.getElementById('number2').value ='' ;
     sum = '';
     document.getElementById('result').innerHTML = sum
 }
