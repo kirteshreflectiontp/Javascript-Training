3f442235bf2e98557a7682ab2c7fcf6d7cdc78f1
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-07-21 10:19:25 +0530

    Google

--- JS(20072022)/Google/Google.js
+++ JS(20072022)/Google/Google.js
@@ -12,29 +12,46 @@ function submit(){
 
     if( firstname =='' && email == "" && password ==""){
         document.getElementById('firsterror').innerHTML = "Enter First and last names";
-        document.getElementById('thirderror').innerHTML = "Choose Gmail address";
+        document.getElementById('thirderror').innerHTML = "Choose a Gmail address";
         document.getElementById('fortherror').innerHTML = "Enter a Password";
+        document.getElementById('info').style.display = "none";
+        document.getElementById('info1').style.display = "none";
+        document.getElementById('firstname').style.border = "2px solid red";
+        document.getElementById('lastname').style.border = "2px solid red";
+        document.getElementById('email').style.border = "2px solid red";
+        document.getElementById('password').style.border = "2px solid red";
+        document.getElementById('confirm').style.border = "2px solid red";
     }
     else if( firstname == ''){
         document.getElementById('firsterror').innerHTML = "Enter Firstname";
+        document.getElementById('firstname').style.border = "2px solid red";
     }
     else if( lastname == ''){
         document.getElementById('seconderror').innerHTML = "Enter Lastname";
+        document.getElementById('lastname').style.border = "2px solid red";
     }
     else if( email == ''){
         document.getElementById('thirderror').innerHTML = "Choose Gmail address";
+        document.getElementById('email').style.border = "2px solid red";
+        document.getElementById('info').style.display = "none";
     }
     else if( password == ''){
         document.getElementById('fortherror').innerHTML = "Enter a Password";
+        document.getElementById('password').style.border = "2px solid red";
+        document.getElementById('info1').style.display = "none";
     }
     else if( password.lenght < 8){
         document.getElementById('fortherror').innerHTML = "Use 8 characters or more";
+        document.getElementById('password').style.border = "2px solid red";
+        document.getElementById('info1').style.display = "none";
     }
     else if( confirm ==''){
         document.getElementById('fiftherror').innerHTML = "Confirm Password";
+        document.getElementById('confirm').style.border = "2px solid red";
+        document.getElementById('info1').style.display = "none";
     }
     else{
-        document.getElementById('done').innerHTML = "Sucessfully Created Your Gmail Account";
+        document.getElementById('done').innerHTML = "You have just created your Gmail account";
     }
     
 }
