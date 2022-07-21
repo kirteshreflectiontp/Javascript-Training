b3a42b27e0bca24ba007dbe62e32b59baf900630
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-07-21 14:04:27 +0530

    switch

---
+++ JS(21072022)/switch.js
@@ -0,0 +1,54 @@
+function cars(id){
+    switch(id){
+        case'newcar':
+        if(document.getElementById(id).style.display =='none'){
+            document.getElementById(id).style.display = 'block';
+        }
+        else{
+            document.getElementById(id).style.display = 'none';
+        }
+        break;
+        case'usedcar':
+        if(document.getElementById(id).style.display =='none'){
+            document.getElementById(id).style.display = 'block';
+        }
+        else{
+            document.getElementById(id).style.display = 'none';
+        }
+        break;
+        case'sellcar':
+        if(document.getElementById(id).style.display =='none'){
+            document.getElementById(id).style.display = 'block';
+        }
+        else{
+            document.getElementById(id).style.display = 'none';
+        }
+        break;
+        case'new&review':
+        if(document.getElementById(id).style.display =='none'){
+            document.getElementById(id).style.display = 'block';
+        }
+        else{
+            document.getElementById(id).style.display = 'none';
+        }
+        break;
+        case'ventures':
+        if(document.getElementById(id).style.display =='none'){
+            document.getElementById(id).style.display = 'block';
+        }
+        else{
+            document.getElementById(id).style.display = 'none';
+        }
+        break;
+        case'more':
+        if(document.getElementById(id).style.display =='none'){
+            document.getElementById(id).style.display = 'block';
+        }
+        else{
+            document.getElementById(id).style.display = 'none';
+        }
+        break;
+
+
+    }
+}
