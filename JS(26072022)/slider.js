57f3fa5db09e67df99ac53b12a2f1a0d3ad70ae2
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-07-27 03:53:36 +0530

    array

---
+++ JS(26072022)/slider.js
@@ -0,0 +1,37 @@
+
+var photos = ['image.png','image2.png','image3'];
+var pics = '';
+for(var a = 0; a < photos.length ;a++){
+    pics +='<div class="photos" id="picture'+a+'"><img src="'+photos[a]+'"/></div>'  
+}
+document.getElementById('imge').innerHTML= pics;
+
+function next(){
+    var slide ='';
+    for(var a=0;a < photos.length;a++){
+        if(document.getElementById('picture'+a).style.display = 'block'){
+            slide = a;
+        }
+        if(document.getElementById('picture'+parseInt(slide+1))){
+            document.getElementById('picture'+a).style.display = 'none';
+        }
+    }
+    if(document.getElementById('picture'+parseInt(slide+1))){
+        document.getElementById('picture'+parseInt(slide+1)).style.display = 'block';
+    }
+}
+
+function back(){
+    var slide ='';
+    for(var a=0;a < photos.length;a++){
+        if(document.getElementById('picture'+a).style.display = 'block'){
+            slide = a;
+        }
+        if(document.getElementById('picture'+parseInt(slide-1))){
+            document.getElementById('picture'+a).style.display = 'none';
+        }
+    }
+    if(document.getElementById('picture'+parseInt(slide-1))){
+        document.getElementById('picture'+parseInt(slide-1)).style.display = 'block';
+    }
+}
