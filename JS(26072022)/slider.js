91d821df595d1432d3008af41ee2552e7a5b5d45
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-07-27 15:18:13 +0530

    array

--- JS(26072022)/slider.js
+++ JS(26072022)/slider.js
@@ -1,5 +1,5 @@
 
-var photos = ['image.png','image2.png','image3'];
+var photos = ['image.png','image2.png','image3.png','footer.png'];
 var pics = '';
 for(var a = 0; a < photos.length ;a++){
     pics +='<div class="photos" id="picture'+a+'"><img src="'+photos[a]+'"/></div>'  
@@ -7,31 +7,33 @@ for(var a = 0; a < photos.length ;a++){
 document.getElementById('imge').innerHTML= pics;
 
 function next(){
+    debugger
     var slide ='';
     for(var a=0;a < photos.length;a++){
-        if(document.getElementById('picture'+a).style.display = 'block'){
+        if(document.getElementById('picture'+a).style.display == 'block'){
             slide = a;
         }
         if(document.getElementById('picture'+parseInt(slide+1))){
             document.getElementById('picture'+a).style.display = 'none';
         }
-    }
-    if(document.getElementById('picture'+parseInt(slide+1))){
+        }
+        if(document.getElementById('picture'+parseInt(slide+1))){
         document.getElementById('picture'+parseInt(slide+1)).style.display = 'block';
+        }
     }
-}
-
+       
 function back(){
-    var slide ='';
-    for(var a=0;a < photos.length;a++){
-        if(document.getElementById('picture'+a).style.display = 'block'){
-            slide = a;
-        }
-        if(document.getElementById('picture'+parseInt(slide-1))){
-            document.getElementById('picture'+a).style.display = 'none';
-        }
-    }
-    if(document.getElementById('picture'+parseInt(slide-1))){
-        document.getElementById('picture'+parseInt(slide-1)).style.display = 'block';
-    }
+debugger
+var slide ='';
+for(var a=0;a < photos.length;a++){
+if(document.getElementById('picture'+a).style.display == 'block'){
+    slide = a;
+}
+if(document.getElementById('picture'+parseInt(slide-1))){
+    document.getElementById('picture'+a).style.display = 'none';
+}
+}
+if(document.getElementById('picture'+parseInt(slide-1))){
+document.getElementById('picture'+parseInt(slide-1)).style.display = 'block';
+}
 }
