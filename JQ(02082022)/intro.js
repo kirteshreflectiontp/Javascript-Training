8a9284b1d7719e08fa849af79b1e0828f1c3f623
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-08-04 01:33:00 +0530

    jquery  events

--- JQ(02082022)/intro.js
+++ JQ(02082022)/intro.js
@@ -1,19 +1,26 @@
 
 
 alert('Click "OK" to Enter')
-$(document).ready(function(){
-    $('.bronco,.truck,.mustang').click(function(){
+
+$(document).ready(function () {
+    $('.bronco,.truck,.mustang').click(function () {
+
+        $('.bronco,.truck,.mustang').removeClass('hover');
+        $(this).addClass('hover');
+
         $('.show-bronco,.show-truck,.show-mustang').hide()
-        if($(this).hasClass('truck')){
-            $('.show-truck').slideToggle();
-        }
-        else if($(this).hasClass('mustang')){
+
+        if ($(this).hasClass('mustang')) {
             $('.show-mustang').slideToggle();
+            $(".mustang").animate({},1000);
         }
-        else{
+        else if ($(this).hasClass('truck')) {
+            $('.show-truck').slideToggle();
+            $(".truck").animate({},1000);
+        }
+        else {
             $('.show-bronco').slideToggle();
+            $(".bronco").animate({},1000);
         }
     })
 })
-
-
---
+++ JQ(04082022)/inputjq.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.input-text').keyup(function(){
+        var text = $(this).val();
+        $('.show-input-text').html(text);
+    })
+    $('.input-text').focus(function(){
+        $(this).css({'background-color':'white'})
+    })
+    $('.input-text').blur(function(){
+        $(this).css({'background-color':'grey'})
+    })
+})
