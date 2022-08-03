2e83b3e5ff1acb5222407fbf8385a373948f3c28
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-08-04 03:36:26 +0530

    animate & Css JQuery

---
+++ JQ(03082022)/animatecss.js
@@ -0,0 +1,19 @@
+alert('efdyfeud')
+$(document).ready(function(){
+    $('.navitem').click(function(){
+        $('.navitem').css({'background-color':'black',color:'red'})
+        $(this).css({'background-color':'blue',color:'white'})
+
+
+        var showtext = $(this).html().toLowerCase();
+
+        $('showitem').css({height:'0',width:'0','background-color':'blue',color:'white'});
+
+        $('.'+showtext).animate({height:'100px',width:'800px'},1000,function(){
+
+        $('.'+showtext).css({'background-color':'green',color:"yellow",'font-weight':'bold'})
+        })
+    })
+})
+
+
